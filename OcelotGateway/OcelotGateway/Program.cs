09a8e9c03ccdbe42ca8415b24918a003cdd4384b
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2023-09-20 21:42:59 +0330

    feat : add ocelot json file

--- OcelotGateway/OcelotGateway/Program.cs
+++ OcelotGateway/OcelotGateway/Program.cs
@@ -1,3 +1,5 @@
+using Ocelot.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Configuration.AddJsonFile("ocelot.json", optional:false, reloadOnChange:true);
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
